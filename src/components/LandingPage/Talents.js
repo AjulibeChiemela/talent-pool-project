f722a240d2ae7a1ee4ac9679b3e9f07e233b489f
Ajulibe Chiemela <chiemelaajulibe@yahoo.com>
2022-07-08 01:54:44 +0100

    finish talents section

--- src/components/LandingPage/Talents.js
+++ src/components/LandingPage/Talents.js
@@ -1,10 +1,54 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./Talents.module.scss";
 const Talents = () => {
   return (
     <div className={classes.talents}>
+      <h2>Meet Our Talents</h2>
+      <ul>
+        <li>
+          <NavLink
+            to="#"
+            className={({ isActive }) =>
+              isActive ? `${classes.active} ${classes.li}` : `${classes.li}`
+            }
+          >
+            Designers
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="##"
+            className={({ isActive }) =>
+              isActive ? `${classes.active} ${classes.li}` : `${classes.li}`
+            }
+          >
+            Developers
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="#"
+            className={({ isActive }) =>
+              isActive ? `${classes.active} ${classes.li}` : `${classes.li}`
+            }
+          >
+            Product Managers
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="#"
+            className={({ isActive }) =>
+              isActive ? `${classes.active} ${classes.li}` : `${classes.li}`
+            }
+          >
+            Operations
+          </NavLink>
+        </li>
+      </ul>
       <div className={classes.talents_card_wrap}>
         <Card className={classes.talents_card}>
           <img src="images/Rectangle17.png" alt="" />
@@ -30,7 +74,7 @@ const Talents = () => {
           <Button className={classes.talents_btn}>Visit Adelanke</Button>
         </Card>
         <Card className={classes.talents_card}>
-          <img src="images/Rectangle17.png" alt="" />
+          <img src="images/Rectangle18.png" alt="" />
           <div className={classes.talents_text}>
             <div>
               <p>Adelanke Adelanke</p>
@@ -53,7 +97,7 @@ const Talents = () => {
           <Button className={classes.talents_btn}>Visit Adelanke</Button>
         </Card>
         <Card className={classes.talents_card}>
-          <img src="images/Rectangle17.png" alt="" />
+          <img src="images/Rectangle19.png" alt="" />
           <div className={classes.talents_text}>
             <div>
               <p>Adelanke Adelanke</p>
